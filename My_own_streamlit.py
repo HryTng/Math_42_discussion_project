9ad0ac1ec3367910f9b0dbdfc79ce7abaa49ce5d
HryTng <129912811+HryTng@users.noreply.github.com>
2023-06-10 13:13:47 -0700

    Update and rename app.py to My_own_streamlit.py

--- My_own_streamlit.py
+++ My_own_streamlit.py
@@ -3,33 +3,29 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import io
 
-web_apps = st.sidebar.selectbox("Select Web Apps",
-                                ("Exploratory Data Analysis", "Distributions"))
+web_apps = st.sidebar.selectbox("Select Web-applications",
+                                ("EDA", "Distribution"))
 
 
-if web_apps == "Exploratory Data Analysis":
+if web_apps == "EDA":
 
   uploaded_file = st.sidebar.file_uploader("Choose a file")
 
   if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
     df = pd.read_csv(uploaded_file)
     show_df = st.checkbox("Show Data Frame", key="disabled")
 
     if show_df:
       st.write(df)
 
-    column_type = st.sidebar.selectbox('Select Data Type',
+    column_type = st.sidebar.selectbox('Select data type',
                                        ("Numerical", "Categorical", "Bool", "Date"))
 
     if column_type == "Numerical":
       numerical_column = st.sidebar.selectbox(
-          'Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
-
-      # histogram
-      choose_color = st.color_picker('Pick a Color', "#69b3a2")
+          'Select a column', df.select_dtypes(include=['int64', 'float64']).columns)
       choose_opacity = st.slider(
-          'Color Opacity', min_value=0.0, max_value=1.0, step=0.05)
+          'Color opacity', min_value=0.0, max_value=1.0, step=0.05)
 
       hist_bins = st.slider('Number of bins', min_value=5,
                             max_value=150, value=30)
@@ -38,7 +34,7 @@ if web_apps == "Exploratory Data Analysis":
 
       fig, ax = plt.subplots()
       ax.hist(df[numerical_column], bins=hist_bins,
-              edgecolor="black", color=choose_color, alpha=choose_opacity)
+              edgecolor="red", alpha=choose_opacity)
       ax.set_title(hist_title)
       ax.set_xlabel(hist_xtitle)
       ax.set_ylabel('Count')
@@ -47,7 +43,6 @@ if web_apps == "Exploratory Data Analysis":
       filename = "plot.png"
       fig.savefig(filename,dpi = 300)
 
-      # Display the download button
       with open("plot.png", "rb") as file:
         btn = st.download_button(
             label="Download image",
